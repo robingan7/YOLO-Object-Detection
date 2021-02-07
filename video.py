807d472ccc6f336efacaa196dcab2081841b2dfe
robingan7 <gzt11111@gmail.com>
2021-02-07 12:21:55 -0800

    new

--- video.py
+++ video.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from scipy import spatial
 
 # Load Yolo model
 net = cv2.dnn.readNet("weights/yolov3.weights", "cfg/yolov3.cfg")
@@ -12,18 +13,43 @@ colors = np.random.uniform(100, 255, size=(len(classes), 3))
 
 videoPath = 'walking.mp4'
 camera = cv2.VideoCapture(videoPath)
+fps = camera.get(cv2.CAP_PROP_FPS)
 _,img = camera.read()
 height, width, channels = img.shape
 
 # Fetching video
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-out = cv2.VideoWriter("output_video.avi", fourcc , 25, (width, height))
+fourcc = cv2.VideoWriter_fourcc(*"MP4V")
+out = cv2.VideoWriter(videoPath.split('.')[0] + 'O.mp4', fourcc , fps, (width, height))
 
 camera = cv2.VideoCapture(videoPath)
+
+conf_threshold = 0.5
+
+lastFrame = {}
+
+def stablizer(last, coord, label, minDistance, isOn):
+    if(not isOn):
+        return coord
+    
+    if(label not in last):
+        return coord
+    
+    points = last[label]
+    tree = spatial.KDTree(points)
+    result = tree.query([coord])
+    
+    if(result[0][0] < minDistance):
+        return points[result[1][0]]
+    
+    return coord
 
 while True:
     _,img = camera.read()
     height, width, channels = img.shape
+
+    if not _:
+        print("End of video")
+        break
 
     # Detecting objects
     blob = cv2.dnn.blobFromImage(img, 0.00392, (320, 320), (0, 0, 0), True, crop=False)
@@ -39,7 +65,7 @@ while True:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5:
+            if confidence > conf_threshold:
                 # Object detected
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
@@ -52,31 +78,41 @@ while True:
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, 0.4)
 
+    currentFrame = {}
     font = cv2.FONT_HERSHEY_SIMPLEX
     for i in range(len(boxes)):
         if i in indexes:
             x, y, w, h = boxes[i]
             label = str(classes[class_ids[i]])
+
+            x, y = stablizer(lastFrame, (x, y), label, 5, True)
+            if(label not in currentFrame):
+                currentFrame[label] = [(x, y)]
+            else:
+                currentFrame[label].append((x, y))
+
+
             color = colors[class_ids[i]]
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
             
             font_scale = 0.7
             thickness = 2
-            text = "{}: {:.1f}%".format(label,
+            text = "{}: {:.0f}%".format(label,
 				confidences[i] * 100)
 
             (text_width, text_height) = cv2.getTextSize(text, font, fontScale=font_scale, thickness=thickness)[0]
 
             # set the text start position
-            text_offset_x = x
-            text_offset_y = y + 20
-
+            text_offset_x, text_offset_y = x, y
+           
             # make the coords of the box with a small padding of two pixels
             txt_box_coords = ((text_offset_x, text_offset_y), (text_offset_x + text_width + 2, text_offset_y - text_height - 8))
             cv2.rectangle(img, txt_box_coords[0], txt_box_coords[1], color, cv2.FILLED)
             cv2.putText(img, text, (text_offset_x + 2, text_offset_y - 5), font, fontScale=font_scale, color=(0, 0, 0), thickness=thickness)
+
+    lastFrame = currentFrame
 
     out.write(img);        
     cv2.imshow("Image", img)
